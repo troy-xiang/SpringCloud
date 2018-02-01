6eb2b08b5cb1220dd5fc729688a441c580d3c2f8
Troy <xiangbo862@163.com>
2018-02-01 15:30:24 +0800

    EurekaServer + EurekaClient

---
+++ spring-cloud-test/eurka-server/src/main/java/com/troy/springcloud/eurkaserver/EurkaServerApplication.java
@@ -0,0 +1,14 @@
+package com.troy.springcloud.eurkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurkaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurkaServerApplication.class, args);
+	}
+}
